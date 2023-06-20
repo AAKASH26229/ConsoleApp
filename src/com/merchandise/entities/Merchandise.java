9d289e5796da3595784bd7fd42458af375170e2b
Aakash <parateakash21.ap@gmail,com>
2023-06-20 10:50:53 +0530

    validation

--- src/com/merchandise/entities/Merchandise.java
+++ src/com/merchandise/entities/Merchandise.java
@@ -2,6 +2,8 @@ package com.merchandise.entities;
 
 public class Merchandise {
 
+	//class “Merchandise” with partnerId, partnerName, City, State properties
+	
 	int partnerId;
 	String partnerName;
 	String City;
@@ -9,11 +11,24 @@ public class Merchandise {
 	
 	public String Validate() {
 		
-		if(partnerId>0 && partnerId!=0) {
+		if( partnerId > 0 && partnerId != 0 ) {
+			
 			return "Partner Id is Mandatory and Non-Negative";
 		}
+		else if( partnerName != null && partnerName.length() >= 5 ) {
+			
+			return "Partner Name is mandatory and have minimum 5 characters";
+		}
+		else if( City != null && City.length() >= 3 ) {
+			
+			return "City is mandatory and have minimum 3 characters";
+		}
+		else if( StateProperties != null && StateProperties.length() >= 3) {
+			
+			return "State is mandatory and have minimum 3 characters";
+		}
+		return "Enter details carefully";
 		
-		return "error";
 	}
 
 }
