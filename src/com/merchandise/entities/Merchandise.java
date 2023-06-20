87e3539ff5483f3f39e1b7e75452b9288da86e50
Aakash <parateakash21.ap@gmail,com>
2023-06-20 16:57:25 +0530

    Adding Validations

--- src/com/merchandise/entities/Merchandise.java
+++ src/com/merchandise/entities/Merchandise.java
@@ -1,33 +1,54 @@
 package com.merchandise.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Merchandise {
 
 	//class “Merchandise” with partnerId, partnerName, City, State properties
 	
-	int partnerId;
-	String partnerName;
-	String City;
-	String StateProperties;
+	protected int partnerId;
+	protected String partnerName;
+	protected String City;
+	protected String StateProperties;
 	
-	public String Validate() {
+	
+	
+	public Merchandise() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public Merchandise(int partnerId, String partnerName, String city, String stateProperties) {
+		super();
+		this.partnerId = partnerId;
+		this.partnerName = partnerName;
+		City = city;
+		StateProperties = stateProperties;
+	}
+
+
+	public String[] Validate() {
+		List<String> errors = new ArrayList<>();
 		
 		if( partnerId > 0 && partnerId != 0 ) {
 			
-			return "Partner Id is Mandatory and Non-Negative";
+			errors.add("Partner Id is mandatory and must be a positive number");
 		}
 		else if( partnerName != null && partnerName.length() >= 5 ) {
 			
-			return "Partner Name is mandatory and have minimum 5 characters";
+			errors.add("Partner Name is mandatory and have minimum 5 characters");
 		}
 		else if( City != null && City.length() >= 3 ) {
 			
-			return "City is mandatory and have minimum 3 characters";
+			errors.add("City is mandatory and have minimum 3 characters");
 		}
 		else if( StateProperties != null && StateProperties.length() >= 3) {
 			
-			return "State is mandatory and have minimum 3 characters";
+			errors.add("State is mandatory and have minimum 3 characters");
 		}
-		return "Enter details carefully";
+		return errors.toArray(new String[0]);
 		
 	}
 
