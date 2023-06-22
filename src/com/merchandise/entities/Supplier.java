91d47e42503552d7c10e08b4783785aa5da38859
Aakash <parateakash21.ap@gmail,com>
2023-06-22 09:31:15 +0530

    supplier validation

--- src/com/merchandise/entities/Supplier.java
+++ src/com/merchandise/entities/Supplier.java
@@ -1,9 +1,74 @@
 package com.merchandise.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Supplier extends Merchandise{
 
 	//For Supplier class: CreditBalance, DrivingLicenseNumber
 	double CreditBalance;
 	long DrivingLicenseNumber;
 	
+	
+	public Supplier(double creditBalance, long drivingLicenseNumber) {
+		super();
+		CreditBalance = creditBalance;
+		DrivingLicenseNumber = drivingLicenseNumber;
+	}
+	
+	// Function to validate
+    // Indian driving license number
+    // using regular expression
+    public static boolean isValidLicenseNo(String str)
+    {
+        // Regex to check valid
+        // Indian driving license number
+        String regex = "^(([A-Z]{2}[0-9]{2})"
+                       + "( )|([A-Z]{2}-[0-9]"
+                       + "{2}))((19|20)[0-9]"
+                       + "[0-9])[0-9]{7}$";
+ 
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+ 
+        // If the string is empty
+        // return false
+        if (str == null) {
+            return false;
+        }
+ 
+        // Find match between given string
+        // and regular expression
+        // uSing Pattern.matcher()
+ 
+        Matcher m = p.matcher(str);
+ 
+        // Return if the string
+        // matched the ReGex
+        return m.matches();
+    }
+		
+		@Override
+		public String[] Validate() {
+		List<String> error = new ArrayList<>();
+		
+//		   // Invoke base class validation
+//        String[] baseValidationErrors = super.Validate();
+//        for (String error : baseValidationErrors) {
+//            errors.add(error);
+//        }
+		
+		if( CreditBalance <= 175000 ) {
+			error.add("Credit Balance not exceed 1,75,000.");
+		}
+		else if()
+
+		return null;
+		
+	}
+	
+	
 }
+
